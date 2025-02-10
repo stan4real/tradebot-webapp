c51936d178a92ff878cd1c35a6c039c38b592d64
Konstantin <st4nshashkov@yandex.ru>
2025-02-10 14:59:23 +0300

    removed base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/tradebot-webapp/",
+  base: "/",
   plugins: [react(), tailwindcss()]
 })
